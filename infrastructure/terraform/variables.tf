3d029f08bf45d1aba63cad0d28e6736bc32e9af8
Magnus Ternström <magnus@falle.se>
2025-02-03 20:57:15 +0100

    Create variables.tf

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = "fc778605-59f1-4fa6-9f4f-838f91bf9d59"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  default     = "477d0a40-9bcf-45a2-8368-afff7c8bca25"
+}
+
+variable "resource_group_name" {
+  description = "price-hunter-rg"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "Sweden Central"
+}
+
+variable "app_service_plan_name" {
+  description = "price-hunter-plan"
+  type        = string
+}
+
+variable "web_app_name" {
+  description = "price-hunter"
+  type        = string
+}
