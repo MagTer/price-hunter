4cc5633c9a9e04d3b0aa5aec18a35c1e6ee5675f
Magnus Ternström <magnus@falle.se>
2025-02-03 20:57:44 +0100

    Create main.tf

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = var.web_app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    always_on = true
+  }
+}
